1e5415c2a019456556bb6d5cf74841965f800fa3
maaz7213 <maaz0301301@gamil.com>
2023-08-22 21:25:04 +0500

    update node modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
+
+import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-    </div>
+      <Header/>
+      <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+   </Routes>
+   </div>
+
+
+
   );
 }
 
---
+++ src/Pages/Header/Header.tsx
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+export default function PersistentDrawerLeft() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
+
+  console.log(screenWidth)
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex', height:'70px' }}>
+      <AppBar position="fixed" open={open} style={{backgroundColor:'rgba(30, 30, 30, 1)'}}>
+        <Toolbar >
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+         {screenWidth<= drawerWidth ?(<>
+            <Typography variant="h6" noWrap component="div" >
+            ANIMEMANIA
+           </Typography>
+         </>):(<>
+
+         </>)}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        
+      >
+        <DrawerHeader style={{backgroundColor:'rgba(30, 30, 30, 1)',}} >
+          {screenWidth>= drawerWidth ?(<>
+            <Typography variant="h6" noWrap component="div" style={{color:'white'}} >
+            ANIMEMANIA
+           </Typography>
+         </>):(<>
+         
+         </>)}
+          <IconButton onClick={handleDrawerClose} style={{color:'white'}}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List style={{backgroundColor:'rgba(30, 30, 30, 1)' ,height:'100%'}}>
+          <h5 style={{color:'white',padding :'20px'}}> Home </h5>
+        </List>
+        </Drawer>
+      <Main open={open}   style={{marginTop:'-50'}}>
+        <DrawerHeader />
+      </Main>
+    </Box >
+  );
+}
